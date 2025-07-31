5ab70f8057fec0c03b53e46cdbbafdfacdc26b12
anuja632 <abianu632.com>
2025-07-31 19:06:28 +0530

    adsdsa

---
+++ js/script.js
@@ -0,0 +1,46 @@
+  $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      dots: false,
+      nav: false,
+      mouseDrag: false,
+      autoplay: true,
+      animateOut: 'slideOutUp',
+      responsive: {
+        0: { items: 1 },
+        600: { items: 1 },
+        1000: { items: 1 }
+      }
+    });
+
+  // Accrodion
+  if ($(".accrodion-grp").length) {
+    var accrodionGrp = $(".accrodion-grp");
+    accrodionGrp.each(function () {
+      var accrodionName = $(this).data("grp-name");
+      var Self = $(this);
+      var accordion = Self.find(".accrodion");
+      Self.addClass(accrodionName);
+      Self.find(".accrodion .accrodion-content").hide();
+      Self.find(".accrodion.active").find(".accrodion-content").show();
+      accordion.each(function () {
+        $(this)
+          .find(".accrodion-title")
+          .on("click", function () {
+            if ($(this).parent().hasClass("active") === false) {
+              $(".accrodion-grp." + accrodionName)
+                .find(".accrodion")
+                .removeClass("active");
+              $(".accrodion-grp." + accrodionName)
+                .find(".accrodion")
+                .find(".accrodion-content")
+                .slideUp();
+              $(this).parent().addClass("active");
+              $(this).parent().find(".accrodion-content").slideDown();
+            }
+          });
+      });
+    });
+  }
+
+ 
